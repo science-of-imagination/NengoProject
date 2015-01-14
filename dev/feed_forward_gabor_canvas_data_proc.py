6a7b16787cbc62f1edcb60d8e45c6fb05c635aa7
Sterling <sterlingsomers@gmail.com>
2015-01-14 17:11:30 -0500

    added patches.

--- dev/feed_forward_gabor_canvas_data_proc.py
+++ dev/feed_forward_gabor_canvas_data_proc.py
@@ -3,7 +3,7 @@ from utils.visualize import figure_from_vector
 from matplotlib.pyplot import show, savefig, clf
 
 path = './model_outputs/feed_forward_gabor_canvas/'
-filename = '20150114135508mmnist0_synapse_itegrater_4000_bigger_gobor'
+filename = '20150114170247patches_1_4105'
 extension = '.pkl.gz'
 data = load_data(path+filename+extension)
 #for i in range(len(data.data)):
@@ -11,7 +11,7 @@ data = load_data(path+filename+extension)
 #    savefig(path+'/imgs/'+filename+'/'+str(i)+'.png')
 #    print 'Saved img '+str(i)
 #    clf()
-avg = sum(data.data[100:])/len(data.data[100:])
+avg = sum(data.data[:100])/len(data.data[0:100])
 figure_from_vector(avg, 28)
 show()
 print 'done'
--- dev/utils/encoders.py
+++ dev/utils/encoders.py
@@ -2,11 +2,40 @@
 
 from numpy import array, linspace, meshgrid, cos, sin, exp, pi, identity
 from numpy.linalg import norm
-from random import uniform
+from random import uniform, choice
 
 def pixel_encoders(canvas_size):
     return identity(canvas_size**2)
 
+def patch(canvas_size, radius, x_offset, y_offset, sign):
+    x = linspace(-1, 1, canvas_size)
+    y = linspace(-1, 1, canvas_size)
+    X, Y = meshgrid(x, y)
+    X = X - x_offset
+    Y = Y + y_offset
+
+    cosTheta = cos(0)
+    sinTheta = sin(0)
+    xTheta = X * cosTheta  + Y * sinTheta
+    yTheta = -X * sinTheta + Y * cosTheta
+    e = exp( -(xTheta**2 + yTheta**2) / (2 *radius**2) )    
+    return sign*e
+
+def make_random_patch(canvas_size):
+    sigma = uniform(0.4, 0.8)    
+    r = (uniform(0,1)-sigma)**2
+    th = uniform(0, 2*pi) 
+    sign = choice([-1,1])
+    return patch(canvas_size, sigma, r*cos(th), r*sin(th), sign)
+
+def normalized_random_patch_encoders(canvas_size, array_size):
+    '''Return an array of random gabor filters placed on a square canvas
+    of side length canvas_size px.
+    '''
+    gabors = [make_random_patch(canvas_size).flatten()
+              for i in range(array_size)]
+    #normalize patches and return
+    return array([(1/norm(i).flatten())*i for i in gabors])    
 
 def gabor(canvas_size, lambd, theta, psi, sigma, gamma, x_offset, y_offset):
     '''Returns a single gabor filter.
