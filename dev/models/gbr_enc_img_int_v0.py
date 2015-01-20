bb8359ff8f5af602f80c8eadb9528241f02949a2
cmekik <can.mekik@gmail.com>
2015-01-20 16:06:30 -0500

    fixed error

--- dev/models/gbr_enc_img_int_v0.py
+++ dev/models/gbr_enc_img_int_v0.py
@@ -12,7 +12,7 @@ def run(N, img_path, w, h):
     
     img = load_img(img_path, dims)
 
-    encs = 
+    encs = normalized_random_gabor_encoders(N, len(img))
 
     def stim_func(t):
         if t<0.1:
