3252b9f236c55a80db700ad0ff2ec73529048e84
cmekik <can.mekik@gmail.com>
2015-01-20 15:59:39 -0500

    tweak to render_data

--- dev/render_data.py
+++ dev/render_data.py
@@ -2,6 +2,8 @@ from tkFileDialog import askopenfilenames
 from utils.visualize import mk_imgs
 from data import load_data
 
-queue = askopenfilenames().split()
+queue = askopenfilenames()
+if isinstance(queue, str):
+    queue = queue.split()
 for path in queue:
     mk_imgs(path.strip('.pkl.gz')+'/', load_data(path))
